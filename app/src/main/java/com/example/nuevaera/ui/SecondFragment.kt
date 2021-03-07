8b4be4d94e6da61733a563e11a46674ef48ebd5c
Leon RO <leon.rodriguez.ore@gmail.com>
2021-03-07 19:51:17 -0300

    Aplicacion de venta de equipo funcionando ok hasta el segundo request probar cambiando el metodo de traer la info

--- app/src/main/java/com/example/nuevaera/ui/SecondFragment.kt
+++ app/src/main/java/com/example/nuevaera/ui/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nuevaera.ui
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import com.example.nuevaera.R
 import com.example.nuevaera.databinding.FragmentSecondBinding
 
